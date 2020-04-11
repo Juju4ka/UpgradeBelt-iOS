fa85587ac82dd5f66911673230c8fc2e49a30917
Julia Boichentsova <ya.anomaly@gmail.com>
2020-04-11 16:05:41 +0100

    Initial project

---
+++ UpgradeBelt/UpgradeBelt/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  UpgradeBelt
+//
+//  Created by Julia Boichentsova on 11/04/2020.
+//  Copyright © 2020 Julia Boichentsova. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UpgradeBelt/UpgradeBelt/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  UpgradeBelt
+//
+//  Created by Julia Boichentsova on 11/04/2020.
+//  Copyright © 2020 Julia Boichentsova. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
