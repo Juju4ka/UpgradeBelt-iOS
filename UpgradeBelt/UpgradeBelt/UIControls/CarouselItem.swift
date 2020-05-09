9dd5356fbb9ea9f7f4f006a574e7d7d4fe4e7ce9
Julia Boichentsova <ya.anomaly@gmail.com>
2020-05-09 01:00:03 +0100

    Added Page View Control

---
+++ UpgradeBelt/UpgradeBelt/UIControls/CarouselItem.swift
@@ -0,0 +1,45 @@
+//
+//  CarouselItem.swift
+//  UpgradeBelt
+//
+//  Created by Julia Boichentsova on 07/05/2020.
+//  Copyright © 2020 Julia Boichentsova. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CarouselItem: UIView {
+    static let CAROUSEL_ITEM_NIB = "CarouselItem"
+    
+    @IBOutlet var vwContent: UIView!
+    @IBOutlet var vwBackground: UIView!
+    @IBOutlet var lblTitle: UILabel!
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initWithNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initWithNib()
+    }
+    
+    convenience init(titleText: String? = "", background: UIColor? = .red) {
+        self.init()
+        lblTitle.text = titleText
+        vwBackground.backgroundColor = background
+    }
+    
+    fileprivate func initWithNib() {
+        Bundle.main.loadNibNamed(CarouselItem.CAROUSEL_ITEM_NIB, owner: self, options: nil)
+        vwContent.frame = bounds
+        vwContent.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(vwContent)
+    }
+}
+
