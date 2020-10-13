954457c7f5d233db2c0e19801585a65ed660071f
Julia Boichentsova <ya.anomaly@gmail.com>
2020-10-13 16:13:22 +0100

    Implemented displaying a list of requirements in a tableview with rounded corners: - items in tableview are not selectable - update height of tableview depending on content size - update scroll state of tableview depending on content size

--- UpgradeBelt/UpgradeBelt/GradingMaterial/RequirementsViewController.swift
+++ UpgradeBelt/UpgradeBelt/GradingMaterial/RequirementsViewController.swift
@@ -18,38 +18,47 @@ class RequirementsViewController: UIViewController, UITableViewDelegate, UITable
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
-    
-    @IBOutlet var textView: UITextView!
-    @IBOutlet var backgroundView: UIView!
+        
+    @IBOutlet var backgroundView: UIView!    
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var tableHeightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
         self.title = "Requirements"
         
+        self.requirements = selectedBelt?.requirements ?? []
+        
+        let lightGreenColor = UIColor(red: 189.0/255.0, green: 234.0/255.0, blue: 167.0/255.0, alpha: 0.8)
+        self.backgroundView.backgroundColor = lightGreenColor
+        
         // Register the table view cell class and its reuse id
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
 
+        self.tableView.backgroundColor = .white
         
-        let lightGreenColor = UIColor(red: 189.0/255.0, green: 234.0/255.0, blue: 167.0/255.0, alpha: 1.0)
-        
-        self.backgroundView.backgroundColor = lightGreenColor
-        self.tableView.backgroundColor = .clear
+        self.tableView.layer.cornerRadius = 10
+        self.tableView.layer.masksToBounds = true
+        self.tableView.allowsSelection = false
         
         // no lines where there aren't cells
         self.tableView.tableFooterView = UIView(frame: .zero)
-     
-        self.requirements = selectedBelt?.requirements ?? []
-        
-//        let chars: Set<Character> = ["\t"]
-//        var text = selectedBelt?.requirements ?? ""
-//        text.removeAll(where: { chars.contains($0) })
-//
-//        self.requirements = text.components(separatedBy: "\n")
-        
+
+        self.tableView.addObserver(self, forKeyPath: "contentSize", options: NSKeyValueObservingOptions(rawValue: 0), context: nil)
         self.tableView.reloadData()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
+        if (self.tableView.contentSize.height < self.tableView.frame.size.height) {
+            self.tableView.isScrollEnabled = false
+        } else {
+            self.tableView.isScrollEnabled = true
+        }
+        
+        tableHeightConstraint.constant = self.tableView.contentSize.height + 50
+        self.tableView.layoutIfNeeded()
     }
     
     // TableView Delegate
@@ -67,8 +76,17 @@ class RequirementsViewController: UIViewController, UITableViewDelegate, UITable
         let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
         
         let requirement = self.requirements[indexPath.row]
+        
+        let image = UIImage(named: "list")
+        cell.imageView?.image = image
+        cell.imageView?.contentMode = .scaleAspectFit
+        
         cell.textLabel?.text = requirement.name
-        cell.textLabel?.numberOfLines = 2
+        cell.textLabel?.numberOfLines = 0
+        
+        cell.backgroundColor = .clear
+//        cell.separatorInset = UIEdgeInsets(top: 2, left: cell.separatorInset.left, bottom: 0, right: 0)
+        
         return cell
     }
     
