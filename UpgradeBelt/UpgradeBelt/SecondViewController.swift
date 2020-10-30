3696ed610337f194105929a0ea089205ef30d59f
Julia Boichentsova <ya.anomaly@gmail.com>
2020-10-30 19:42:53 +0000

    About page: updated description, email and website

--- UpgradeBelt/UpgradeBelt/SecondViewController.swift
+++ UpgradeBelt/UpgradeBelt/SecondViewController.swift
@@ -19,7 +19,7 @@ class SecondViewController: UIViewController, MFMailComposeViewControllerDelegat
         // Do any additional setup after loading the view.
                 
         textView.contentInset = .init(top: 0, left: 20, bottom: 0, right: 20)
-        textView.text = "Upgrade Belt app is created in order to help Taekwon-Do students to study the terminology and patterns of Taekwon-Do in preparation for grading.\n\nIf you'd like to contribute or support the project please contact us."
+        textView.text = "Upgrade Belt app is created for Taekwon-Do students to browse and learn grading material in preparation for their next belt. \n\nIf you'd like to contribute or support the project please contact us."
     }
     
     func openURL(url: URL) {
@@ -36,7 +36,7 @@ class SecondViewController: UIViewController, MFMailComposeViewControllerDelegat
     }
     
     @IBAction func contactUs(_ sender:UIButton) {
-        let recipientEmail = "thegirlinafloraldress@gmail.com"
+        let recipientEmail = "upgradebelt@gmail.com"
         let subject = "Upgrade Belt"
         let body = ""
 
@@ -55,7 +55,7 @@ class SecondViewController: UIViewController, MFMailComposeViewControllerDelegat
             if(UIApplication.shared.canOpenURL(emailUrl)) {
                 UIApplication.shared.open(emailUrl)
             } else {
-                let url = URL(string:"https://thegirlinafloraldress.blogspot.com")
+                let url = URL(string:"https://upgradebelt.wordpress.com/")
                 self.openURL(url: url!)
             }
         }
